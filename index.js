03c2a0345d7728db51998a65a77bf75765730be9
dci-student <alinacuznetov@gmail.com>
2022-09-23 14:18:43 +0200

    solution added

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ console.log(randomNum);
 
 let counter = 0;
 
-
 const guess = prompt("Guess a number between 1 - 10:");
-
-console.log(guess);
 
 
 
@@ -17,19 +14,18 @@ for (let i = 0; i < 2; i++) {
         break;
     } else {
         counter++;
-        //console.log("Counter is:", counter);
+
         const guess2 = prompt("Wrong, try again!");
-        //console.log("input2 is:", input2);
 
         if (guess2 === randomNum && counter !== 3) {
             counter++;
+
             alert(`Success, the number was ${randomNum}! Attempts: ${counter}`);
             break;
         } else if (guess !== randomNum && counter === 2) {
             alert(`Sorry, you failed to guess the number in 3 attempts. The number was ${randomNum}!`)
         }
     }
-
 
 }
 
